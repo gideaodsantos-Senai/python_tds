383e2dba62ed05eb8157e0c70e393e6e11bdf944
Gideão dos Santos <gideao.d.santos@aluno.senai.br>
2026-01-28 15:49:10 -0300

    atualização

---
+++ exercicio11.py
@@ -0,0 +1,9 @@
+print("Calculadora de Resistor Limitador")
+
+fonte = float(input("Insira a tensão da fonte de alimentação: "))
+operacao = float(input("Insira a tensão de operação do dispositivo/LED: "))
+correnDese = float(input("Corrente desejada: "))
+
+resistor = fonte - operacao / correnDese
+
+print(f"O resistor necessário é {resistor} Ohms")
---
+++ exercicio12.py
@@ -0,0 +1,29 @@
+print("Exércicio 1 - Calculo de 2 números inteiros")
+
+num1 = int(input("Insira o número 1: "))
+num2 = int(input("Insira o número 2: "))
+
+soma = num1 + num2
+produto = num1 * num2
+
+print(f"A soma dos dois valores são {soma}\n O produto dos dois valores é {produto}")
+
+
+
+print("Exércicio 2 - Conversão de metros em centímetros")
+
+metros = int(input("Insira a sua estatura em metros: "))
+conversao = metros * 100
+
+print(f"{metros} metros equivale a {conversao} centimetros")
+
+
+
+print("Exércicio 3 - Geometria Plana")
+
+base = float(input("Insira a base: "))
+altura = float(input("Insira a altura: "))
+
+area = base * altura / 2
+
+print(f"A área da Geometria é {area}")
